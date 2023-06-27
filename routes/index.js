cb93c9da2c1a19139c744e7c0a3c986ef05a2c56
Adrien NEYRON <adrien.neyron@gmail.com>
2023-06-27 12:30:34 +0200

    debut des routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+const newTrip = new Trip ({ departure: req.body.departure, arrival: req.body.arrival, date: req.body.date});
+
+//route pour aller chercher les information du client
+
+router.post('/',(req,res) => {
+  req.body.departure, req.body.arrival, req.body.date === 
+  res.json({tripsList: Trips})
+});
+
+
+
+//route pour creer la base de donnée utilisateur et enregistrer les billets
+router.post('/')
+
+
+
+
 module.exports = router;
