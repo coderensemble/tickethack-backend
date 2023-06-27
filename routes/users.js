63624032344b9c8d5a2d448aa0d41caa355dfeb7
maximegenand <m.genand@gmx.fr>
2023-06-27 17:00:36 +0200

    add put & delete routes

--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,32 @@ var express = require('express');
 var router = express.Router();
 const Ticket = require('../models/tickets');
 
-/* GET panier du user */
-router.get('cart/:user', (req, res) => {
-  console.log(req.params);
-  /*const { user } = req.params;
-  const tickets = Ticket.find({ user, paid: false });
-  return tickets.length > 0 ? res.json({ result: true, tickets }) : res.json({ result: false });*/
+/* GET - Panier du user */
+router.get('/cart/:user', async (req, res) => {
+  const { user } = req.params;
+  const tickets = await Ticket.find({ user, paid: false });
+  return tickets.length > 0 ? res.json({ result: true, tickets }) : res.json({ result: false });
 });
 
-/* GET commandes validées du user */
-router.get('bookings/:user', async (req, res) => {
+/* GET - Commandes validées du user */
+router.get('/bookings/:user', async (req, res) => {
   const { user } = req.params;
   const tickets = await Ticket.find({ user, paid: true });
   return tickets.length > 0 ? res.json({ result: true, tickets }) : res.json({ result: false });
+});
+
+/* PUT - Mise à jour du paiement du user */
+router.put('/buy/:user', async (req, res) => {
+  const { user } = req.params;
+  const result = await Ticket.updateMany({ user, paid: false }, { paid: true });
+  return res.json({ result: result.acknowledged });
+});
+
+/* DELETE - Suppression d'un billet */
+router.delete('/delete/:ticket', async (req, res) => {
+  const { ticket } = req.params;
+  const result = await Ticket.deleteOne({ _id: ticket });
+  return result.deletedCount === 1 ? res.json({ result: true }) : res.json({ result: false });
 });
 
 module.exports = router;
